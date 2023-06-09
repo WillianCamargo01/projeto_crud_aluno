665c821f0ffa68e20521d96f736f3d828ef5dc56
WillianCamargo01 <william.yury@outlook.com>
2023-06-09 10:10:33 -0300

    Atualizacao Crendencias DB Aws

--- conexao.php
+++ conexao.php
@@ -1,15 +1,14 @@
 <?php
-// Definir as credenciais de conexão com o banco de dados
-$servername = "localhost";
-$username = "root";
-$password = "o@rf4zvX*Rrv5XP.";
-$dbname = "aluno";
+// Credenciais de conexão com o banco de dados
+$servername = "db-ads.c8bqy6anulng.sa-east-1.rds.amazonaws.com";
+$username = "admin";
+$password = "Unimar-ads-2023";
+$dbname = "minha_base";
 
-// Criar a conexão com o banco de dados
+// Cria a conexão com o banco de dados
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Verificar se a conexão foi estabelecida com sucesso
+// Verifica se a conexão foi estabelecida com sucesso
 if ($conn->connect_error) {
     die("Erro na conexão com o banco de dados: " . $conn->connect_error);
 }
-?>
